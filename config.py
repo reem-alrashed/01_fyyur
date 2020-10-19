a22145201d4f367aa46b3a0851b73f296c84cf8e
Reem <reemalrashed2@gmail.com>
2020-10-19 12:14:03 +0300

    Database connection handeling added

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ DEBUG = True
 
 # Connect to the database
 
-
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = "postgres://{}@{}/{}".format(
+    "reem", '127.0.0.1:5432', "fyyur")
+SQLALCHEMY_TRACK_MODIFICATIONS = True'
+
+
